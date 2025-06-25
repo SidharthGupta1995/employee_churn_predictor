4d59ae8347a9fd8a8ef5453beba89bdf8bfb9cfd
Sidharth Gupta <sidharth.gupta@technodataanalytics.com>
2025-06-25 17:04:18 -0400

    Updated model and added new demo sample files

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,26 +18,26 @@ st.title("Employee Churn Prediction")
 
 uploaded_file = st.file_uploader("Upload Employee Data (CSV)", type=["csv"])
 
-# Ensure categorical columns are strings
-categorical_columns = ['Role','Gender','Shift_Type','Reason_for_Leaving',
-                       'Job_Satisfaction_Category','Last_Increment_Bracket']
-for col in categorical_columns:
-    df[col] = df[col].astype(str).fillna("Missing")
-
-# Ensure numeric columns are numeric
-numeric_columns = ['Age', 'Experience_Years','Distance_km', 'Salary_Hourly','Engagement_Score',
-                   'Tenure_Months','Satisfaction_Level', 'Avg_Monthly_Hours','Work_Accident', 
-                   'Promotion_Last_2Years','Salary_Change_%','Overtime_Hours_Week',
-                   'Days_Since_Last_Leave','HR_Complaint', 'Conflict_Reported', 'Years_in_Current_Role']
-for col in numeric_columns:
-    df[col] = pd.to_numeric(df[col], errors="coerce")
-
-# Drop rows with missing data (optional, can be changed to fillna)
-df.dropna(inplace=True)
-
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
     st.write("### Uploaded Data Preview", df.head())
+    
+    # Ensure categorical columns are strings
+    categorical_columns = ['Role','Gender','Shift_Type','Reason_for_Leaving',
+                           'Job_Satisfaction_Category','Last_Increment_Bracket']
+    for col in categorical_columns:
+        df[col] = df[col].astype(str).fillna("Missing")
+
+    # Ensure numeric columns are numeric
+    numeric_columns = ['Age', 'Experience_Years','Distance_km', 'Salary_Hourly','Engagement_Score',
+                       'Tenure_Months','Satisfaction_Level', 'Avg_Monthly_Hours','Work_Accident', 
+                       'Promotion_Last_2Years','Salary_Change_%','Overtime_Hours_Week',
+                       'Days_Since_Last_Leave','HR_Complaint', 'Conflict_Reported', 'Years_in_Current_Role']
+    for col in numeric_columns:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+
+    # Drop rows with missing data (optional, can be changed to fillna)
+    df.dropna(inplace=True)
 
     # Run prediction
     if st.button("Predict Churn"):
